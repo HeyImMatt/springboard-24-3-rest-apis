ad43c83328d892d08b7d9bccf45df937abf15528
heyimmatt <mrmcfarland@gmail.com>
2020-08-19 15:37:13 -0700

    Replace bad apostrophe and quote characters, and install linters

---
+++ app.py
@@ -0,0 +1,17 @@
+"""Flask Cupcakes Exercise"""
+
+from flask import Flask, request, redirect, render_template
+from flask_debugtoolbar import DebugToolbarExtension
+from models import db, connect_db
+
+app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///cupcakes'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_ECHO'] = True
+app.config['SECRET_KEY'] = 'cupcakes1234'
+app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
+debug = DebugToolbarExtension(app)
+
+connect_db(app)
+db.create_all()
