49f0a60db828113c50ad4cde16950c5c41f51c44
heyimmatt <mrmcfarland@gmail.com>
2020-08-19 15:46:06 -0700

    Add Cupcake model

--- models.py
+++ models.py
@@ -7,3 +7,14 @@ def connect_db(app):
 
     db.app = app
     db.init_app(app)
+
+class Cupcake(db.Model):
+    """Cupcake Model"""
+
+    __tablename__ = 'cupcakes'
+
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    flavor = db.Column(db.String, nullable=False)
+    size = db.Column(db.String(50), nullable=False)
+    rating = db.Column(db.Float, nullable=False)
+    image = db.Column(db.String, nullable=False, default='https://tinyurl.com/demo-cupcake')
