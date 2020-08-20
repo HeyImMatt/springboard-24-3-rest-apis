06952eff538511c8cbda4eb37c8423c512501225
heyimmatt <mrmcfarland@gmail.com>
2020-08-20 10:40:51 -0700

    Add cupcake form and js to handle adding cupcake

--- static/scripts.js
+++ static/scripts.js
@@ -1,6 +1,18 @@
+document.querySelector('#createBtn').addEventListener('click', addCupcake)
+
 async function fetchAllCupcakes() {
   const resp = await axios.get('/api/cupcakes')
   return resp.data.cupcakes
+}
+
+async function createCupcake(cupcake) {
+  try {
+    await axios.post('/api/cupcakes', cupcake)
+    location.reload();
+  }
+  catch {
+    alert('Cupcake not added')
+  }
 }
 
 
@@ -20,6 +32,28 @@ async function renderCupcakes() {
     li.append(img)
     cupcakesList.append(li);
   });
+}
+
+function addCupcake(e) {
+  e.preventDefault();
+  
+  const flavorInput = document.querySelector('#flavor');
+  const sizeInput = document.querySelector('#size');
+  const ratingInput = document.querySelector('#rating');
+  const imageInput = document.querySelector('#image');
+
+  createCupcake({
+    flavor: flavorInput.value,
+    size: sizeInput.value,
+    rating: ratingInput.value,
+    image: imageInput.value
+  })
+
+  flavorInput.value = '';
+  sizeInput.value = '';
+  ratingInput.value = '';
+  imageInput.value = '';
+
 }
 
 renderCupcakes()
